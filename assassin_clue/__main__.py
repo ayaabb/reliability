098a8b885afbbe7789cbd22dcc1c7de6358fdcb6
ayaabb <ayaabbas011@gmail.com>
2024-03-21 02:39:34 +0200

    added tests and docstring file

--- assassin_clue/__main__.py
+++ assassin_clue/__main__.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+from utils.save_docstring import save_class_docstrings
 from classes.class_game import Game
 from players_init import get_players
 from utils.load_package import merge_data, load_json_files
@@ -8,25 +9,18 @@ from utils.load_package import merge_data, load_json_files
 
 def main():
     places = merge_data(load_json_files("places"), 'places')
-    players = get_players()
+    players_names = merge_data(load_json_files("players"), 'names')
+    weapons = merge_data(load_json_files("weapons"), 'weapons')
+    players = get_players(players_names, weapons)
     game = Game(players, places)
     game.initialize_game()
     game.Start()
 
 
-def test():
-    f = 0
-    while f < 2:
-        if f == 2:
-            raise TypeError("ok")
-        f += 1
-
-    return "R"
+def save_docstring_to_file(class_, output_file_path):
+    save_class_docstrings(class_, output_file_path)
 
 
 if __name__ == "__main__":
-    # try:
-    #     l = test()
-    # except TypeError as e:
-    #     print(e)
-    main()
+    save_docstring_to_file(Game, 'docstring_file.txt')
+    # main()
--- assassin_clue/players_init/get_players.py
+++ assassin_clue/players_init/get_players.py
@@ -2,15 +2,12 @@ import os
 import random
 
 # from  utils.load_package import *
-from  classes.class_player import player
+from classes.class_player import player
 
 from utils.load_package import *
 
 
-def get_players():
-
-    players_names = merge_data(load_json_files("players"), 'names')
-    weapons = merge_data(load_json_files(  "weapons"), 'weapons')
+def get_players(players_names, weapons):
     players = []
     for name in players_names:
         players.append(player(name, random.sample(weapons, random.randint(2, 4))))
